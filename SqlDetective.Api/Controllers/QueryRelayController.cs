b26d97ad10adc2ab2d39ad4821cf05b68ba76100
ofek <ofekcofman98@gmail.com>
2026-02-01 22:55:52 +0200

    added PostgresQueryExecutionService to the controller

--- SqlDetective.Api/Controllers/QueryRelayController.cs
+++ SqlDetective.Api/Controllers/QueryRelayController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using SqlDetective.Domain.Query.Service;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
+using Newtonsoft.Json.Linq;
+using SqlDetective.Data.Postgres.Query;
 
 
 namespace SqlDetective.Api.Controllers
@@ -12,11 +14,13 @@ namespace SqlDetective.Api.Controllers
     public class QueryRelayController : ControllerBase
     {
         private readonly IQueryRelayService r_QueryRelayService;
+    private readonly PostgresQueryExecutionService r_QueryExecutionService;
         private readonly ILogger<QueryRelayController> r_Logger;
 
-        public QueryRelayController(IQueryRelayService i_QueryRelayService, ILogger<QueryRelayController> i_Logger)
+        public QueryRelayController(IQueryRelayService i_QueryRelayService, ILogger<QueryRelayController> i_Logger, PostgresQueryExecutionService i_QueryExecutionService)
         {
             r_QueryRelayService = i_QueryRelayService;
+            r_QueryExecutionService = i_QueryExecutionService;
             r_Logger = i_Logger;
         }
 
@@ -52,21 +56,21 @@ namespace SqlDetective.Api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetNextQuery([FromQuery] string key, CancellationToken ct)
         {
-            //r_Logger.LogInformation("[QueryRelay] [GET] starting GetNextQuery");
+      //r_Logger.LogInformation("[QueryRelay] [GET] starting GetNextQuery");
 
-            if (string.IsNullOrEmpty(key))
-            {
-                return BadRequest("Missing Key");
-            }
+      if (string.IsNullOrEmpty(key)) return BadRequest("Missing Key");
 
-            string? queryJson = await r_QueryRelayService.GetNextQueryForPcAsync(key, ct);
+      string? sqlQuery = await r_QueryRelayService.GetNextQueryForPcAsync(key, ct);
 
-            if (queryJson == null)
-            {
-                return NoContent();
-            }
 
-            return Content(queryJson, "application/json");
-        }
+      if (sqlQuery == null)
+      {
+        return NoContent(); // אין שאילתה חדשה כרגע
+      }
+      JArray queryResults = await r_QueryExecutionService.ExecuteAsync(key, sqlQuery, ct);
+
+      return Ok(queryResults);
+      //return Content(queryJson, "application/json");
+      }
     }
 }
